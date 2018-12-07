e95746d2f87cd7f125b5c68c7f469b6cf4c8a6c0
Peter Pierce <peter@truenorth.us>
2018-12-07 18:27:05 -0500

    solved 2018/05b

--- solutions/2018/05.py
+++ solutions/2018/05.py
@@ -4,6 +4,7 @@ Puzzles 5a and 5b for 2018
 See https://adventofcode.com/2018/day/5
 """
 import os
+import string
 
 from common import util
 
@@ -11,35 +12,62 @@ INPUT_DIR = os.path.join(os.path.dirname(__file__), 'inputs')
 
 
 def _reactive(a, b):
-    """Determine if two polymers (letters) will react."""
+    """Determine if two units (letters) will react."""
     return bool(abs(ord(a) - ord(b)) == 32)
+
+
+def _reduce(material):
+    """reduce a material to its stable form by causing reactions."""
+    reduced, done = list(material), False
+
+    while not done:
+        reduced = [c for c in reduced if c]
+        i, i_max, done = 0, len(reduced) - 1, True
+
+        while i < i_max:
+            if _reactive(reduced[i], reduced[i+1]):
+                reduced[i], reduced[i+1] = None, None
+                done = False
+                i += 1
+            i += 1
+
+    return ''.join(reduced)
+
+
+def _strip_unit_type(a, material):
+    """Remove both polarities of a unit type (e.g., bB) from a material."""
+    unit_type = (a.lower(), a.upper())
+    return ''.join([c for c in material if c not in unit_type])
 
 
 def run():
     answer, done = {'a': {}, 'b': {}}, False
     start_time = util.now()
-    
-    with open(os.path.join(INPUT_DIR, 'input05.dat'), 'r') as f:
-        material = f.read().strip()
-        
-    while not done:
-        material = [c for c in material if c]
-        i, i_max = 0, len(material) - 1
-        done = True
-        
-        while i < i_max:
-            if _reactive(material[i], material[i+1]):
-                material[i], material[i+1] = None, None
-                done = False
-                i += 1
-            i += 1
-        
-    answer['a'].update({'length': len(material), 'material': ''.join(material)})
 
+    with open(os.path.join(INPUT_DIR, 'input05.dat'), 'r') as f:
+        initial_material = f.read().strip()
+
+    # part A
+
+    reduced_material = _reduce(initial_material)
+    answer['a'].update({'length': len(reduced_material), 'material': ''.join(reduced_material)})
     print('Part One answer is: %d' % answer['a']['length'])
-    
+
     # part B
-    
+
+    attempts = []
+    for letter in string.ascii_lowercase:
+        print('reducing less %s' % letter)
+        stripped_and_reduced = _reduce(_strip_unit_type(letter, initial_material))
+        attempts.append({'unit_type': letter, 'size': len(stripped_and_reduced)})
+
+    ranked = sorted(attempts, key=lambda d: d['size'])
+    answer['b'].update(ranked[0])
+
+    print('Part Two answer is: %d' % answer['b']['size'])
+
+    # summary
+
     print('whole answer dict is: %s' % str(answer))
     print('  elapsed: %f seconds' % util.elapsed_since(start_time))
 
